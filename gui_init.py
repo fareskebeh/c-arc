4a2fb73113d80d42aa20cb98a44c44b7181148e1
fareskebeh <fares.kebbeh@gmail.com>
2025-10-15 12:41:09 +0300

    specified a bridge class to interact with the JavaScript API

--- gui_init.py
+++ gui_init.py
@@ -1,8 +1,21 @@
 import webview
-from pathlib import Path
+import os
 
-def initialize(window):
-    window.maximize()
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+html_path = os.path.join(BASE_DIR,"UI", "layout.html")
 
-window =webview.create_window('C-arc', frameless=True)
-webview.start(initialize, window)
+class Bridge:
+    def close(self):
+        webview.windows[0].destroy()
+    def restore(self):
+        webview.windows[0].resize(700,500)
+    def maximize(self):
+        webview.windows[0].maximize()
+    def minimize(self):
+        webview.windows[0].minimize()
+        
+bridge=Bridge()
+
+webview.create_window('C-arc', f"file://{html_path}", frameless=True, width=700, height=500 ,resizable=True, js_api=bridge)
+
+webview.start()
