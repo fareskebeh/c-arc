7b91d931129827e80a83a5753b98026579e6e3d2
fareskebeh <fares.kebbeh@gmail.com>
2025-10-16 13:02:45 +0300

    Eliminated the custom title bar

--- gui_init.py
+++ gui_init.py
@@ -5,17 +5,11 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 html_path = os.path.join(BASE_DIR,"UI", "layout.html")
 
 class Bridge:
-    def close(self):
-        webview.windows[0].destroy()
-    def restore(self):
-        webview.windows[0].resize(700,500)
-    def maximize(self):
-        webview.windows[0].maximize()
-    def minimize(self):
-        webview.windows[0].minimize()
+    pass
+
         
 bridge=Bridge()
 
-webview.create_window('C-arc', f"file://{html_path}", frameless=True, width=700, height=500 ,resizable=True, js_api=bridge)
+webview.create_window('C-arc', f"file://{html_path}", width=700, height=500 ,resizable=True, js_api=bridge)
 
 webview.start()
